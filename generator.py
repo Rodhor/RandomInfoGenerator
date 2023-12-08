7cb2a2b0f95d94e0d85593759fb9c350c4ccb77a
Chris Nielsen <chris.rahbek.nielsen@gmail.com>
2023-12-08 10:09:52 +0100

    Implemented download functionality, refactored and removed unnecessary code/functions, and addressed bugs. Improved code readability with added comments.

--- generator.py
+++ generator.py
@@ -2,50 +2,67 @@ from faker import Faker
 import pandas as pd
 import random
 
-fake = Faker(['DE_de'])
+# Create a Faker object with the German locale
+fake = Faker(["DE_de"])
 
 
-def generate_data(selection_data_dict, number_of_entries=10) -> list:
-
-    selection_items = [item['name'].lower().replace(' ', '_')
-                       for item in selection_data_dict if 'name' in item]
-    generated_data = {item: [getattr(fake, item)()
-                             for _ in range(number_of_entries)] for item in selection_items}
+# Function to generate synthetic data based on a selection dictionary
+def generate_data(selection_data_dict, number_of_entries) -> dict:
+    # Extract 'name' entries from the selection data and convert to lowercase with underscores
+    selection_items = [
+        item["name"].lower().replace(" ", "_")
+        for item in selection_data_dict
+        if "name" in item
+    ]
+    # Generate synthetic data using Faker for each selected item
+    generated_data = {
+        item: [getattr(fake, item)() for _ in range(number_of_entries)]
+        for item in selection_items
+    }
     return generated_data
 
 
-def generate_custom_data(custom_data_dict, number_of_entries=10) -> dict:
+# Function to generate custom synthetic data based on a custom data dictionary
+def generate_custom_data(custom_data_dict, number_of_entries) -> dict:
+    # Iterate through the custom data dictionary
     for key, value in custom_data_dict.items():
         generated_entries = []
+        # Generate synthetic data for each custom item
         for _ in range(number_of_entries):
             generated_value = ""
+            # Replace '?' with a random uppercase letter and '#' with a random digit
             for char in value:
-                if char == '?':
+                if char == "?":
                     generated_value += fake.random_letter().upper()
-                elif char == '#':
+                elif char == "#":
                     generated_value += str(random.randint(0, 9))
                 else:
                     generated_value += char
-
             generated_entries.append(generated_value)
-
         custom_data_dict[key] = generated_entries
 
     return custom_data_dict
 
 
-def generate_dataframe(column_names_list, custom_data_dict, selection_data) -> pd.DataFrame:
-    # List comprehension to retrieve keys from each dictionary
+# Function to generate a DataFrame with synthetic data
+def generate_dataframe(
+    column_names_list, custom_data_dict, selection_data, number_of_entries
+) -> pd.DataFrame:
     try:
-        column_names = [dictionary['name'].lower().replace(' ', '_')
-                        for dictionary in column_names_list]
-        selection = generate_data(selection_data)
-        custom = generate_custom_data(custom_data_dict)
+        # Extract 'name' entries from the column_names_list and convert to lowercase with underscores
+        column_names = [
+            dictionary["name"].lower().replace(" ", "_")
+            for dictionary in column_names_list
+        ]
+        # Generate synthetic data for both selection and custom data
+        selection = generate_data(selection_data, number_of_entries)
+        custom = generate_custom_data(custom_data_dict, number_of_entries)
+        # Combine selection and custom data into a single dictionary
         data = {**selection, **custom}
+        # Create a DataFrame using column names and synthetic data
+        df = pd.DataFrame(columns=column_names, data=data)
 
-        df = pd.DataFrame(columns=column_names, data=data).head()
-
-    # Handel exeption of user inputerror. Page will reload and error will not occur again - therefor just pass
+    # Handle exceptions related to user input errors, page reloads to prevent recurrence
     except:
         pass
 
--- main.py
+++ main.py
@@ -1,109 +1,203 @@
+# Import necessary libraries
 import streamlit as st
 from st_draggable_list import DraggableList
 import pandas as pd
-from generator import generate_dataframe
+from generator import generate_dataframe  # Assuming a custom generator module
+import io
 
 
+# Function to initialize session state with default values
 def initialize_session_state(default_values) -> None:
     for key, value in default_values.items():
         if key not in st.session_state:
             st.session_state[key] = value
+    load_css()
 
 
-def load_css() -> list:
-    pass
+# Function to load CSS styles
+@st.cache_data
+def load_css() -> None:
+    # Define CSS files for different pages
+    css_files = {"input": "input.css", "order": "order.css", "preview": "preview.css"}
+
+    for key, file_path in css_files.items():
+        with open(file_path, "r") as file:
+            content = file.read()
+            st.session_state.css[key] = content
 
 
-def change_page() -> None:
-    pass
+# Function to change the current page
+def change_page(page) -> None:
+    st.session_state.page = page
 
 
+# Function to set page style using loaded CSS
+def set_page():
+    st.markdown(
+        f"<style>{st.session_state.css[st.session_state.page]}</style>",
+        unsafe_allow_html=True,
+    )
+
+
+# Function to create a dictionary based on user inputs for custom data
 def customdata_dict() -> dict:
-    # Retrieve values from the custom input option, save in a list for labels and formatting respectively ---
-    labels = [st.session_state.labels[i] for i in range(5)]
-    format = [st.session_state.format[i] for i in range(5)]
-
-    # Generate a dictionary with label als formatting as key, value pairs
+    # Retrieve user input for column labels and formats
+    labels = [st.session_state.labels[i] for i in range(7)]
+    format = [st.session_state.format[i] for i in range(7)]
     return dict(zip(labels, format))
 
 
+# Function to create a list of dictionaries based on user selections for selection data
 def selectiondata_dict() -> dict:
-    # List comprehension to get selected items with their order - list of dictionaries
-    return [{'name': key, 'order': order} for order, (key, selected) in enumerate(
-        st.session_state.selection.items()) if selected]
+    return [
+        {"name": key, "order": order}
+        for order, (key, selected) in enumerate(st.session_state.selection.items())
+        if selected
+    ]
 
 
+# Function to compile dictionaries into one based on user selections
 def compile_dictionary(custom_items, selection_items) -> dict:
-
-    # Combine the dictionaries into one - selected_items is unpacked and placed first in the new dictionary
-    return {**{item['name']: item['order'] for item in selection_items}, **custom_items}
+    return {**{item["name"]: item["order"] for item in selection_items}, **custom_items}
 
 
+# Function to compile a draggable list based on user selections and custom data
 def compile_draggablelist() -> dict:
-
     selection_items = selectiondata_dict()
     custom_items = customdata_dict()
-
-    # call dictionary generator to retrieve necessary values
     combined_dict = compile_dictionary(custom_items, selection_items)
+    return [
+        {"name": key, "order": order}
+        for order, (key, item) in enumerate(combined_dict.items())
+        if item != "" and key != ""
+    ]
 
-    # Use listcomprehension to generate a new list of dictionaries, for each item in original - keeping only the order and key values.
-    return [{'name': key, 'order': order} for order, (key, item) in enumerate(combined_dict.items()) if item != "" and key != ""]
 
-
+# Function to generate a DataFrame with synthetic data based on user inputs
 def generate_data_in_dataframe(column_names_list) -> pd.DataFrame:
     custom_data = customdata_dict()
     selection_data = selectiondata_dict()
-    return generate_dataframe(column_names_list, custom_data, selection_data)
+    return generate_dataframe(column_names_list, custom_data, selection_data, entries)
 
 
-def download_data() -> None:
-    pass
+# Function to generate BytesIO objects for CSV, Excel, and Pickle files
+def generate_download_files(df):
+    # CSV
+    csv_file = io.BytesIO()
+    df.to_csv(csv_file, index=False)
+    csv_file.seek(0)
+
+    # Excel
+    excel_file = io.BytesIO()
+    with pd.ExcelWriter(excel_file, engine="openpyxl") as excel_writer:
+        df.to_excel(excel_writer, index=False, sheet_name="Sheet1")
+    excel_file.seek(0)
+
+    # Pickle
+    pickle_file = io.BytesIO()
+    df.to_pickle(pickle_file)
+    pickle_file.seek(0)
+
+    return csv_file, excel_file, pickle_file
 
 
-# --- Set default variables ---
+# Set default values for the session state
 default_values = {
-    'css': [],
-    'selection': {},
-    'labels': {},
-    'format': {},
-    'selectionitems': ['First Name', 'Last name', 'Date of birth', 'Country', 'Address', 'City', 'Postcode', 'Current Country', 'Phone number', 'Job']
+    "css": {},
+    "page": "input",
+    "selection": {},
+    "labels": {},
+    "format": {},
+    "selectionitems": [
+        "First Name",
+        "Last name",
+        "Date of birth",
+        "Country",
+        "Address",
+        "City",
+        "Postcode",
+        "Current Country",
+        "Phone number",
+        "Job",
+    ],
+    "dataset": "",
 }
 
-# --- Initialize session with default values ---
+# Initialize session with default values
 initialize_session_state(default_values)
 
-# --- custimize page with css file ---
-with open("selection.css") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+# Set the CSS style for the current page
+set_page()
 
-# --- Selection page ---
-# generate columenview of ease of navigation
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.markdown('### **Select fields**')
-    for item in st.session_state.selectionitems:
-        st.session_state.selection[item] = st.toggle(item)
+# Selection page
+with st.container():
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.markdown("### **Select fields**")
+        for item in st.session_state.selectionitems:
+            st.session_state.selection[item] = st.toggle(item)
 
-with col2:
-    st.markdown('### **Column label**')
-    for i in range(5):
-        st.session_state.labels[i] = st.text_input(
-            label=f"{i}", placeholder=f'label {i+1}', key=f'label_input_{i}'
+    with col2:
+        st.markdown("### **Column label**")
+        for i in range(7):
+            st.session_state.labels[i] = st.text_input(
+                label=f"{i}", placeholder=f"label {i+1}", key=f"label_{i}"
+            )
+
+    with col3:
+        st.markdown("### **Format**")
+        for i in range(7):
+            st.session_state.format[i] = st.text_input(
+                label=f"{i}", placeholder="Input format", key=f"format_{i}"
+            )
+
+    entries = st.number_input(
+        "Number of rows", min_value=1000, max_value=100_000, step=1000
+    )
+
+    selections_made = any(st.session_state.selection.values())
+    inputs_provided = any(st.session_state.labels.values()) and any(
+        st.session_state.format.values()
+    )
+
+    button_disabled = not (selections_made or inputs_provided)
+
+    st.button(
+        "Order",
+        key="input_to_order",
+        on_click=lambda: change_page("order"),
+        disabled=button_disabled,
+    )
+
+# Order of items page
+with st.container():
+    slist = DraggableList(compile_draggablelist())
+    col1, col2 = st.columns(2)
+    col2.button(
+        "Preview", key="order_to_preview", on_click=lambda: change_page("preview")
+    )
+    col1.button("Input", key="order_to_input", on_click=lambda: change_page("input"))
+
+# Dataframe preview and download
+with st.container():
+    if st.session_state.page == "preview":
+        df = generate_data_in_dataframe(slist)
+
+        csv, excel, pickle = generate_download_files(df)
+
+        col1, col2, col3 = st.columns(3)
+        col1.download_button("Download CSV", data=csv, file_name="Dataset.csv")
+        col2.download_button("Download Excel", data=excel, file_name="Dataset.xlsx")
+        col3.download_button("Download Pickle", data=pickle, file_name="Dataset.pkl")
+        st.dataframe(df.head(), hide_index=True, use_container_width=True)
+
+        st.button(
+            "Change order",
+            key="preview_to_order",
+            on_click=lambda: change_page("order"),
         )
-
-with col3:
-    st.markdown('### **Format**')
-    for i in range(5):
-        st.session_state.format[i] = st.text_input(
-            label=f"{i}", placeholder='Input format', key=f'input_{i}'
+        st.button(
+            "Change input",
+            key="preview_to_input",
+            on_click=lambda: change_page("input"),
         )
-
-
-# --- Order of items page ---
-slist = DraggableList(compile_draggablelist())
-
-
-# --- Dataframe preview and download ---
-if st.button('ok'):
-    st.dataframe(generate_data_in_dataframe(slist), hide_index=True)
